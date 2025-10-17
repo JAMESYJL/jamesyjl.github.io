e6e215e6f5802984486d171331b19a2f76df4d2c
Junliang Ye <145889120+JAMESYJL@users.noreply.github.com>
2025-10-17 12:11:40 +0800

    Update generation.js

--- Nano3D/js/generation.js
+++ Nano3D/js/generation.js
@@ -3,15 +3,15 @@ var edit_items = [
     video:"../nano3d/video/2/src.mp4", 
     video2:"../nano3d/video/2/tar.mp4",
     model:"assets_glb/../nano3d/video/2/src.glb", 
-    model2:"../assets_glb/../nano3d/video/2/tar.glb",
+    model2:"assets_glb/../nano3d/video/2/tar.glb",
     source:"Nano3D", 
     text:"Make the backpack the boy is carrying bigger." },
 
     {
     video:"../nano3d/video/4/src.mp4", 
     video2:"../nano3d/video/4/tar.mp4",
-    model:"../assets_glb/../nano3d/video/4/src.glb", 
-    model2:"../assets_glb/../nano3d/video/4/tar.glb",
+    model:"assets_glb/../nano3d/video/4/src.glb", 
+    model2:"assets_glb/../nano3d/video/4/tar.glb",
     source:"Nano3D", 
     text:"Replace the blue jacket with a brown down jacket." },
 
@@ -26,136 +26,136 @@ var edit_items = [
     {
     video:"../nano3d/video/3/src.mp4", 
     video2:"../nano3d/video/3/tar.mp4",
-    model:"../assets_glb/../nano3d/video/3/src.glb", 
-    model2:"../assets_glb/../nano3d/video/3/tar.glb",
+    model:"assets_glb/../nano3d/video/3/src.glb", 
+    model2:"assets_glb/../nano3d/video/3/tar.glb",
     source:"Nano3D", 
     text:"Replace the spherical radar dome with a cylindrical container." },
 
     {
     video:"../nano3d/video/5/src.mp4", 
     video2:"../nano3d/video/5/tar.mp4",
-    model:"../assets_glb/../nano3d/video/5/src.glb", 
-    model2:"../assets_glb/../nano3d/video/5/tar.glb",
+    model:"assets_glb/../nano3d/video/5/src.glb", 
+    model2:"assets_glb/../nano3d/video/5/tar.glb",
     source:"Nano3D", 
     text:"Remove the wings." },
 
     {
     video:"../nano3d/video/6/src.mp4", 
     video2:"../nano3d/video/6/tar.mp4",
-    model:"../assets_glb/../nano3d/video/6/src.glb", 
-    model2:"../assets_glb/../nano3d/video/6/tar.glb",
+    model:"assets_glb/../nano3d/video/6/src.glb", 
+    model2:"assets_glb/../nano3d/video/6/tar.glb",
     source:"Nano3D", 
     text:"Holding a sword." },
 
     {
     video:"../nano3d/video/7/src.mp4", 
     video2:"../nano3d/video/7/tar.mp4",
-    model:"../assets_glb/../nano3d/video/7/src.glb", 
-    model2:"../assets_glb/../nano3d/video/7/tar.glb",
+    model:"assets_glb/../nano3d/video/7/src.glb", 
+    model2:"assets_glb/../nano3d/video/7/tar.glb",
     source:"Nano3D", 
     text:"Replace the marble lamp base with a faceted geometric one." },
 
     {
     video:"../nano3d/video/8/src.mp4", 
     video2:"../nano3d/video/8/tar.mp4",
-    model:"../assets_glb/../nano3d/video/8/src.glb", 
-    model2:"../assets_glb/../nano3d/video/8/tar.glb",
+    model:"assets_glb/../nano3d/video/8/src.glb", 
+    model2:"assets_glb/../nano3d/video/8/tar.glb",
     source:"Nano3D", 
     text:"Add a satellite dish on top of the house" },
 
     {
     video:"../nano3d/video/9/src.mp4", 
     video2:"../nano3d/video/9/tar.mp4",
-    model:"../assets_glb/../nano3d/video/9/src.glb", 
-    model2:"../assets_glb/../nano3d/video/9/tar.glb",
+    model:"assets_glb/../nano3d/video/9/src.glb", 
+    model2:"assets_glb/../nano3d/video/9/tar.glb",
     source:"Nano3D", 
     text:"Remove the chair" },
 
     {
     video:"../nano3d/video/10/src.mp4", 
     video2:"../nano3d/video/10/tar.mp4",
-    model:"../assets_glb/../nano3d/video/10/src.glb", 
-    model2:"../assets_glb/../nano3d/video/10/tar.glb",
+    model:"assets_glb/../nano3d/video/10/src.glb", 
+    model2:"assets_glb/../nano3d/video/10/tar.glb",
     source:"Nano3D", 
     text:"Replacing the eagle head with lion head" },
 
     {
     video:"../nano3d/video/11/src.mp4", 
     video2:"../nano3d/video/11/tar.mp4",
-    model:"../assets_glb/../nano3d/video/11/src.glb", 
-    model2:"../assets_glb/../nano3d/video/11/tar.glb",
+    model:"assets_glb/../nano3d/video/11/src.glb", 
+    model2:"assets_glb/../nano3d/video/11/tar.glb",
     source:"Nano3D", 
     text:"Replace the short straight hair with a fluffy middle-parted hairstyle." },
 
     {
     video:"../nano3d/video/12/src.mp4", 
     video2:"../nano3d/video/12/tar.mp4",
-    model:"../assets_glb/../nano3d/video/12/src.glb", 
-    model2:"../assets_glb/../nano3d/video/12/tar.glb",
+    model:"assets_glb/../nano3d/video/12/src.glb", 
+    model2:"assets_glb/../nano3d/video/12/tar.glb",
     source:"Nano3D", 
     text:"Add a padlock to he chest." },
 
     {
     video:"../nano3d/video/13/src.mp4", 
     video2:"../nano3d/video/13/tar.mp4",
-    model:"../assets_glb/../nano3d/video/13/src.glb", 
-    model2:"../assets_glb/../nano3d/video/13/tar.glb",
+    model:"assets_glb/../nano3d/video/13/src.glb", 
+    model2:"assets_glb/../nano3d/video/13/tar.glb",
     source:"Nano3D", 
     text:"Replace the hat with a helmet." },
 
     {
     video:"../nano3d/video/14/src.mp4", 
     video2:"../nano3d/video/14/tar.mp4",
-    model:"../assets_glb/../nano3d/video/14/src.glb", 
-    model2:"../assets_glb/../nano3d/video/14/tar.glb",
+    model:"assets_glb/../nano3d/video/14/src.glb", 
+    model2:"assets_glb/../nano3d/video/14/tar.glb",
     source:"Nano3D", 
     text:"Add a shield to the penguin’s left hand." },
 
     {
     video:"../nano3d/video/15/src.mp4", 
     video2:"../nano3d/video/15/tar.mp4",
-    model:"../assets_glb/../nano3d/video/15/src.glb", 
-    model2:"../assets_glb/../nano3d/video/15/tar.glb",
+    model:"assets_glb/../nano3d/video/15/src.glb", 
+    model2:"assets_glb/../nano3d/video/15/tar.glb",
     source:"Nano3D", 
     text:"Replace the bottom part of the lamp with a rectangular stone base." },
 
     {
     video:"../nano3d/video/16/src.mp4", 
     video2:"../nano3d/video/16/tar.mp4",
-    model:"../assets_glb/../nano3d/video/16/src.glb", 
-    model2:"../assets_glb/../nano3d/video/16/tar.glb",
+    model:"assets_glb/../nano3d/video/16/src.glb", 
+    model2:"assets_glb/../nano3d/video/16/tar.glb",
     source:"Nano3D", 
     text:"Replace the shield in the character’s hand with a sword." },
 
     {
     video:"../nano3d/video/17/src.mp4", 
     video2:"../nano3d/video/17/tar.mp4",
-    model:"../assets_glb/../nano3d/video/17/src.glb", 
-    model2:"../assets_glb/../nano3d/video/17/tar.glb",
+    model:"assets_glb/../nano3d/video/17/src.glb", 
+    model2:"assets_glb/../nano3d/video/17/tar.glb",
     source:"Nano3D", 
     text:"Remove the handle from the lamp." },
 
     {
     video:"../nano3d/video/18/src.mp4", 
     video2:"../nano3d/video/18/tar.mp4",
-    model:"../assets_glb/../nano3d/video/18/src.glb", 
-    model2:"../assets_glb/../nano3d/video/18/tar.glb",
+    model:"assets_glb/../nano3d/video/18/src.glb", 
+    model2:"assets_glb/../nano3d/video/18/tar.glb",
     source:"Nano3D", 
     text:"Add a basket to the front of the scooter." },
 
     {
     video:"../nano3d/video/19/src.mp4", 
     video2:"../nano3d/video/19/tar.mp4",
-    model:"../assets_glb/../nano3d/video/19/src.glb", 
-    model2:"../assets_glb/../nano3d/video/19/tar.glb",
+    model:"assets_glb/../nano3d/video/19/src.glb", 
+    model2:"assets_glb/../nano3d/video/19/tar.glb",
     source:"Nano3D", 
     text:"Replace the chimney with a bell" },
 
     {
     video:"../nano3d/video/20/src.mp4", 
     video2:"../nano3d/video/20/tar.mp4",
-    model:"../assets_glb/../nano3d/video/20/src.glb", 
-    model2:"../assets_glb/../nano3d/video/20/typical_misc_workbench_2_tar.glb",
+    model:"assets_glb/../nano3d/video/20/src.glb", 
+    model2:"assets_glb/../nano3d/video/20/typical_misc_workbench_2_tar.glb",
     source:"Nano3D", 
     text:"Remove the open drawer from the workbench." },
 ];
